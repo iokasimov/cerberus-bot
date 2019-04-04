5885f55e431a46d51d328c253eab05591caf4ebe
Murat Kasimov <iokasimov.m@gmail.com>
2019-04-04 15:04:53 +0300

    Define dumb webhook in Server to test new version of telega

--- Network/Telegram/API/Bot/Cerberus/Configuration.hs
+++ Network/Telegram/API/Bot/Cerberus/Configuration.hs
@@ -2,16 +2,14 @@ module Network.Telegram.API.Bot.Cerberus.Configuration
 	(Environment, Settings (..), settings) where
 
 import "base" Control.Monad ((>>=))
-import "base" Data.Int (Int, Int64)
-import "base" Data.Function ((.), ($))
+import "base" Data.Int (Int64)
+import "base" Data.Function ((.))
 import "base" Data.Functor ((<$>))
 import "base" Control.Applicative ((<*>))
-import "base" Data.Maybe (Maybe (Nothing))
 import "base" System.IO (IO)
 import "base" Prelude (negate)
 import "optparse-applicative" Options.Applicative (Parser
 	, execParser, argument, auto, info, fullDesc, metavar, str)
-import "stm" Control.Concurrent.STM (TVar, newTVarIO)
 import "telega" Network.Telegram.API.Bot (Token (Token))
 import "text" Data.Text (pack)
 import "wreq" Network.Wreq.Session (Session, newAPISession)
--- Network/Telegram/API/Bot/Cerberus/Server.hs
+++ Network/Telegram/API/Bot/Cerberus/Server.hs
@@ -1,3 +1,37 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Network.Telegram.API.Bot.Cerberus.Server where
+module Network.Telegram.API.Bot.Cerberus.Server (API, server) where
+
+import "async" Control.Concurrent.Async (async)
+import "base" Control.Applicative (pure)
+import "base" Control.Monad.IO.Class (liftIO)
+import "base" Data.Bool ((||))
+import "base" Data.Eq ((/=))
+import "base" Data.Function ((.), ($))
+import "base" Data.Functor (void)
+import "base" System.IO (print)
+import "lens" Control.Lens ((^.))
+import "servant-server" Servant (Capture, ReqBody, Server, JSON, Post, FromHttpApiData, ToHttpApiData, type (:>), err403, throwError)
+import "telega" Network.Telegram.API.Bot (Telegram, Token (Token), telegram)
+import "telega" Network.Telegram.API.Bot.Property (identificator)
+import "telega" Network.Telegram.API.Bot.Object.Message (Message (Textual))
+import "telega" Network.Telegram.API.Bot.Object.Update (Update (Membership, Incoming), chat)
+import "text" Data.Text.IO (putStrLn)
+import "transformers" Control.Monad.Trans.Class (lift)
+
+import Network.Telegram.API.Bot.Cerberus.Configuration (Environment, Settings (Settings))
+
+type API = "webhook" :> Capture "secret" Token :> ReqBody '[JSON] Update :> Post '[JSON] ()
+
+deriving instance ToHttpApiData Token
+deriving instance FromHttpApiData Token
+
+server :: Settings -> Server API
+server (Settings token chat_id session) secret update =
+	if secret /= token || (identificator $ update ^. chat) /= chat_id then throwError err403 else
+		liftIO . void . async . telegram session token chat_id $ webhook update
+
+webhook :: Update -> Telegram Environment ()
+webhook (Incoming _ (Textual _ _ _ txt)) = lift . lift . putStrLn $ txt
+webhook (Membership _ m) = lift . lift . print $ m
+webhook _ = pure ()
