3b5d98d83a5e342d92d6de1e8e3295d05403448a
Murat Kasimov <iokasimov.m@gmail.com>
2019-04-06 14:10:16 +0300

    Implement checking member status

--- Network/Telegram/API/Bot/Cerberus/Database.hs
+++ Network/Telegram/API/Bot/Cerberus/Database.hs
@@ -1,19 +1,25 @@
 module Network.Telegram.API.Bot.Cerberus.Database (Role (..)) where
 
-import "base" Control.Applicative (Applicative ((<*>)))
+import "base" Control.Applicative (Applicative (pure, (<*>)))
+import "base" Data.Function (id, ($), (&))
 import "base" Data.Functor ((<$>))
 import "base" Data.Int (Int)
+import "base" Data.Maybe (maybe)
 import "base" Data.Semigroup (Semigroup ((<>)))
+import "base" System.IO (IO)
 import "base" Text.Show (Show (show))
-import "sqlite-simple" Database.SQLite.Simple (SQLData (SQLInteger))
+import "lens" Control.Lens (element, (^?))
+import "sqlite-simple" Database.SQLite.Simple (Connection, Only (Only), SQLData (SQLInteger), query)
 import "sqlite-simple" Database.SQLite.Simple.Ok (Ok (Ok))
 import "sqlite-simple" Database.SQLite.Simple.Internal (Field (Field))
 import "sqlite-simple" Database.SQLite.Simple.FromField (FromField (fromField), ResultError (ConversionFailed), returnError)
 import "sqlite-simple" Database.SQLite.Simple.FromRow (FromRow (fromRow), field)
 import "sqlite-simple" Database.SQLite.Simple.ToField (ToField (toField))
 import "sqlite-simple" Database.SQLite.Simple.ToRow (ToRow (toRow))
+import "telega" Network.Telegram.API.Bot.Object.From (From)
+import "telega" Network.Telegram.API.Bot.Property.Identifiable (Identifiable (identificator))
 
-data Role = Guest | Partaker
+data Role = Guest | Partaker deriving Show
 
 instance FromField Role where
 	fromField (Field (SQLInteger 0) _) = Ok Guest
@@ -26,10 +32,15 @@ instance ToField Role where
 	toField Guest = SQLInteger 0
 	toField Partaker = SQLInteger 1
 
-data Somebody = Somebody Int Role
+data Somebody = Somebody Int Role deriving Show
 
 instance FromRow Somebody where
 	fromRow = Somebody <$> field <*> field
 
 instance ToRow Somebody where
 	toRow (Somebody tlg_id role) = toRow (tlg_id, role)
+
+check :: From -> Connection -> IO Somebody
+check from connection = do
+	r <- query connection "SELECT * from members where id = ?" (Only $ identificator from)
+	pure $ r ^? element 0 & maybe (Somebody (identificator from) Guest) id
--- Network/Telegram/API/Bot/Cerberus/Server.hs
+++ Network/Telegram/API/Bot/Cerberus/Server.hs
@@ -5,7 +5,7 @@ module Network.Telegram.API.Bot.Cerberus.Server (API, server) where
 import "async" Control.Concurrent.Async (async)
 import "base" Control.Applicative (pure)
 import "base" Control.Monad.IO.Class (liftIO)
-import "base" Data.Bool ((||))
+import "base" Data.Bool (Bool, (||))
 import "base" Data.Eq ((/=))
 import "base" Data.Function ((.), ($))
 import "base" Data.Functor (void)
@@ -14,6 +14,7 @@ import "lens" Control.Lens ((^.))
 import "servant-server" Servant (Capture, ReqBody, Server, JSON, Post, FromHttpApiData, ToHttpApiData, type (:>), err403, throwError)
 import "telega" Network.Telegram.API.Bot (Telegram, Token (Token), telegram)
 import "telega" Network.Telegram.API.Bot.Property (identificator)
+import "telega" Network.Telegram.API.Bot.Object.Member (Member (Gone))
 import "telega" Network.Telegram.API.Bot.Object.Message (Message (Textual))
 import "telega" Network.Telegram.API.Bot.Object.Update (Update (Membership, Incoming), chat)
 import "transformers" Control.Monad.Trans.Class (lift)
@@ -27,10 +28,12 @@ deriving instance FromHttpApiData Token
 
 server :: Settings -> Server API
 server (Settings token chat_id connection session) secret update =
-	if secret /= token || (identificator $ update ^. chat) /= chat_id then throwError err403 else
-		liftIO . void . async . telegram session token (connection, chat_id) $ webhook update
+	if wrong_chat || wrong_token then throwError err403 else
+		liftIO . void . async . telegram session token (connection, chat_id) $ webhook update where
+
+	wrong_chat, wrong_token :: Bool
+	wrong_chat = identificator (update ^. chat) /= chat_id
+	wrong_token = secret /= token
 
 webhook :: Update -> Telegram Environment ()
-webhook i@(Incoming _ (Textual _ _ _ _)) = lift . lift . print $ i
-webhook (Membership _ m) = lift . lift . print $ m
 webhook x = lift . lift . print $ x
